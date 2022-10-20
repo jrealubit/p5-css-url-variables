c2d39e0d7bb60f47980a95a19314ed909a12eed2
jrealubit <44243913+jrealubit@users.noreply.github.com>
2022-10-19 20:37:33 -0500

    adding images

--- p3/sketch.js
+++ p3/sketch.js
@@ -1,7 +1,19 @@
 let clr;
+let img;
+
+function preload(){
+  img = loadImage('whole image.png'); 
+  /*
+  Creator
+  Katsushika Hokusai (Japanese, 1760-1849)
+  Title
+  Fine Wind, Clear Morning (Gaif^u kaisei)
+  */
+}
 
 function setup() {
-  let canvas = createCanvas(200, 200); //create canvas half screen width
+  let canvas = createCanvas(745, 509); //create canvas half screen width
+
   canvas.addClass('positioned'); //add positioned class to canvas (see style.css)
   let params = getParams(); //get values from URL
   clr = params.color; 
@@ -11,5 +23,6 @@ function setup() {
 
 function draw() {
   fill(clr);
-  ellipse(random(200), random(200), 20);
+  ellipse(random(745), random(509), 20);
+  image(img,mouseX,mouseY);
 }
